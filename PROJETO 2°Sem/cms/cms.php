6a44cd7dcbed601c58dc55c8dd6576fdbdc44ad0
DaniloVitor2552 <danilovitor739@gmail.com>
2019-10-08 16:58:23 -0300

    pagina cmd

--- PROJETO 2°Sem/cms/cms.php
+++ PROJETO 2°Sem/cms/cms.php
@@ -9,16 +9,69 @@
     </head>
     
     <body>
-        <div class="conteudo center">
+        
+            <!--  Area conteudo      -->
+        <header class="conteudo center">
+            
+        <div id="area_cms">
             <div id="cms">
                 <div id="titulo_cms">
-                    jakljdjasd
+                    <h2>CMS<span> - Sistema de Gerenciamento do site</span></h2>
+                    
                 </div>
                 
                 <div id="imagem_cms">
                 
                 </div>
             </div>
-        </div> 
+            
+                <!--   Area menu cms     -->
+            
+            <div id="menu_cms">
+                <div class="area_img">
+                    <div class="img">
+                    
+                    </div> 
+                    <p>Adm. Conteúdo</p>
+                </div>
+                
+                <div class="area_img">
+                    <div class="img">
+                    
+                    </div>
+                    <p> Adm. Fale Conosco</p>
+                    
+                </div>
+                
+                <div class="area_img">
+                    <div class="img">
+                    
+                    </div>
+                    
+                    <p>Adm. Usúarios</p>
+                </div>
+                
+                <div id="area_bemvindo">
+                    Bem Vindo, Mermao
+                </div>
+                
+                <div id="logout">
+                   <input type="submit" name="btnlogout" value="Logout"> 
+                </div>
+            </div>
+            
+            <div id="area_conteudo">
+                
+            </div>
+        </div>
+            
+        <!--   Area rodape     -->
+        
+        <footer id="rodape">
+            <h1>Desenvolvido por: Danilo Vitor</h1>
+        </footer>
+            
+        </header>
+        
     </body>
 </html>
